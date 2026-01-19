9f9d951c4a710ad2e965c40f5db6bdc3db1e70a6
AshvinAA <ashvin.afroz@gmail.com>
2026-01-20 00:59:12 +0600

    Module 1

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask ,request , render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+#We are basically telling python that we are building a new web application object by using the Flask blueprint
+#__name__ basically is an in-built tool that tells Python that the web application/class is being used right here in this file 
+app=Flask(__name__)
+
+#telling app that we are using sqlite for our database 
+#The ADDRESS to store the database
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///capsule.db'
+#turning auto tracking off which allows us to access every change in the memory of the database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Initialising the database
+#The DELIVERY of the database
+db=SQLAlchemy(app)
+
+class Capsule(db.Model):
+    # Primary Key: The unique ID for every message (1, 2, 3...)
+    id= db.Column(db.Integer, primary_key=True)
+    message= db.Column(db.Text , nullable=False)
+    unlock_data=db.Column(db.DateTime,nullable=False)
+    created_at=db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Capsule {self.id}>'
+
+
+@app.route('/')
+def home():
+    return "Time Capsule is running"
+
+if __name__ == '__main__':
+    app.run(debug=True)
